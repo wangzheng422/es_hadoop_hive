9ea0ce3522a1f8e5fcbb947e5b282e90f9ce1d0b
wzh <wangzheng422@gmail.com>
2018-06-22 17:04:00 +0800

    hadoop k8s

--- dev/hadoop/yml.sh
+++ dev/hadoop/yml.sh
@@ -11,7 +11,9 @@ sed "s/{{ALB_IP}}/${ALB_IP}/g" | \
 sed "s/{{GIT_HOST}}/${GIT_HOST}/g" | \
 sed "s/{{REGISTRY}}/${REGISTRY}/g" | \
 sed "s/{{REGION}}/${REGION}/g" | \
-sed "s/{{NGINX}}/${NGINX}/g"  \
+sed "s/{{NGINX}}/${NGINX}/g" | \
+sed "s/{{NODE_IPS}}/${NODE_IPS}/g" | \
+sed "s/{{NAME_NODE_ADDR}}/${NAME_NODE_ADDR}/g"  \
 > k8s-tmp.yml
 
 echo "use k8s-tmp.yaml to deploy the app"
