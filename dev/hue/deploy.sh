245f392821794856cfba09f846cf0e4ac963f85b
wzh <wangzheng422@gmail.com>
2018-06-27 10:02:39 +0800

    hue

--- dev/hue/deploy.sh
+++ dev/hue/deploy.sh
@@ -6,15 +6,25 @@ set -x
 source config.sh
 
 
-# cat k8s.yml | \
-# sed "s/{{ALB_IP}}/${ALB_IP}/g" | \
-# sed "s/{{GIT_HOST}}/${GIT_HOST}/g" | \
-# sed "s/{{REGISTRY}}/${REGISTRY}/g" | \
-# sed "s/{{REGION}}/${REGION}/g" | \
-# sed "s/{{NGINX}}/${NGINX}/g" | \
-# sed "s/{{NODE_IPS}}/${NODE_IPS}/g" | \
-# sed "s/{{NAME_NODE_ADDR}}/${NAME_NODE_ADDR}/g"  \
-# > k8s-tmp.yml
+cat conf/hive-site.xml | \
+sed "s/{{ALB_IP}}/${ALB_IP}/g" | \
+sed "s/{{GIT_HOST}}/${GIT_HOST}/g" | \
+sed "s/{{REGISTRY}}/${REGISTRY}/g" | \
+sed "s/{{REGION}}/${REGION}/g" | \
+sed "s/{{NGINX}}/${NGINX}/g" | \
+sed "s/{{NODE_IPS}}/${NODE_IPS}/g" | \
+sed "s/{{NAME_NODE_ADDR}}/${NAME_NODE_ADDR}/g"  \
+> tmp/hive-site.xml
+
+cat conf/pseudo-distributed.ini | \
+sed "s/{{ALB_IP}}/${ALB_IP}/g" | \
+sed "s/{{GIT_HOST}}/${GIT_HOST}/g" | \
+sed "s/{{REGISTRY}}/${REGISTRY}/g" | \
+sed "s/{{REGION}}/${REGION}/g" | \
+sed "s/{{NGINX}}/${NGINX}/g" | \
+sed "s/{{NODE_IPS}}/${NODE_IPS}/g" | \
+sed "s/{{NAME_NODE_ADDR}}/${NAME_NODE_ADDR}/g"  \
+> tmp/pseudo-distributed.ini
 
 # echo "use k8s-tmp.yaml to deploy the app"
 
